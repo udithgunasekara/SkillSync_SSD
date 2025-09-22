2624ca9e471b506c261a3efeaeca3ceab48e0d8a
udithgunasekara <supeshalagunasekara@gmail.com>
2025-09-23 00:14:55 +0530

    common utils

--- BackEnd/src/main/java/BackEnd/BackEnd.java
+++ BackEnd/src/main/java/BackEnd/BackEnd.java
@@ -5,12 +5,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableScheduling
 @RestController
 @CrossOrigin
 //@ComponentScan(basePackages = {"com.example", "BackEnd.service"})
